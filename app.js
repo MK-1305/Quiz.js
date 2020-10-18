88fdf9b9949a84c51cdedc984d77076a759726b7
松浦一樹 <kzkdrmlove@gmail.com>
2020-10-18 23:31:27 +0900

    q1

---
+++ app.js
@@ -0,0 +1,24 @@
+const question = 'ゲーム市場、最も売れたゲーム機は次の内どれ？';
+const answers = [
+  'スーパーファミコン',
+  'プレイステーション2',
+  'ニンテンドースイッチ',
+  'ニンテンドーDS'
+];
+const correct = 'プレイステーション2';
+//定数の文字列をHTMLに反映させる
+document.getElementById('js-question').textContent = question;
+
+document.getElementsByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[0].textContent = answers[1];
+document.getElementsByTagName('button')[0].textContent = answers[2];
+document.getElementsByTagName('button')[0].textContent = answers[3];
+
+//ボタンをクリックしたら正誤判定
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+  if(correct === document.getElementsByTagName('button')[0].textContent = answers[0];){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+})
